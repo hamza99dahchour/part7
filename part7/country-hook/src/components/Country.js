0abb0077b19ac6bbccc17ecf7ce20c196aff6171
hamza99dahchour <106910048+hamza99dahchour@users.noreply.github.com>
2022-06-05 17:01:36 +0100

    Add files via upload

---
+++ part7/country-hook/src/components/Country.js
@@ -0,0 +1,31 @@
+
+const Country = ({ country }) => {
+  
+  if (!country) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+      
+    }
+  
+    if (!country.found) {
+      return (
+        <div>
+          not found...
+        </div>
+      )
+    }
+  
+    return (
+      <div>
+        <h3>{country.name} </h3>
+        <div>capital {country.data.capital} </div>
+        <div>population {country.data.population}</div> 
+        <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      </div>
+    )
+}
+
+export default Country
